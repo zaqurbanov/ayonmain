63b2346e6c793211e78f248c06cf06299f99747e
zaqurbanov <111583648+zaqurbanov@users.noreply.github.com>
2023-02-19 15:32:48 +0300

    Add files via upload
    Signed-off-by: zaqurbanov <111583648+zaqurbanov@users.noreply.github.com>

---
+++ scrol.js
@@ -0,0 +1,13 @@
+ScrollReveal({reset:true}); 
+
+ScrollReveal().reveal('.logo',{delay:500 });
+ScrollReveal().reveal('.left-row',{delay:500 });
+ScrollReveal().reveal('.right-row',{delay:1000 });
+ScrollReveal().reveal('.page-header h2',{delay:1000 });
+
+
+ScrollReveal().reveal('.list',{delay:100});
+ScrollReveal().reveal('.mySwiper',{delay:100})
+ScrollReveal().reveal('.footer',{delay:150});
+ScrollReveal().reveal('.contact',{delay:200});
+ScrollReveal().reveal('.footer-list',{delay:300})

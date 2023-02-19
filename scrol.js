12f4285b3f6c6b98f8631881bfd76edff2cff9da
zaqurbanov <111583648+zaqurbanov@users.noreply.github.com>
2023-02-19 16:30:36 +0300

    Add files via upload
    Signed-off-by: zaqurbanov <111583648+zaqurbanov@users.noreply.github.com>

--- scrol.js
+++ scrol.js
@@ -11,3 +11,8 @@ ScrollReveal().reveal('.mySwiper',{delay:100})
 ScrollReveal().reveal('.footer',{delay:150});
 ScrollReveal().reveal('.contact',{delay:200});
 ScrollReveal().reveal('.footer-list',{delay:300})
+ScrollReveal().reveal('.i1',{delay:300})
+ScrollReveal().reveal('.i2',{delay:500})
+ScrollReveal().reveal('.i3',{delay:700})
+ScrollReveal().reveal('.i4',{delay:900})
+ScrollReveal().reveal('.i5',{delay:1100})
